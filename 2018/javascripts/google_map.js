ee1a49df28ed694fd014572591cd967ede6cebd8
yongjoo <contact.yongjoo@gmail.com>
2018-10-30 15:38:21 +0900

    js쪼개기

---
+++ 2018/javascripts/google_map.js
@@ -0,0 +1,35 @@
+function myMap() {
+
+  //// 지도 그리기
+  var mapProp= {
+      center:new google.maps.LatLng(37.463651, 126.954269), //지도 중앙값 
+      zoom:15,
+  };
+  var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+  //// 마커 표시하기
+  var marker = new google.maps.Marker({
+    position: {lat: 37.463651, lng: 126.954269},
+    animation:google.maps.Animation.BOUNCE
+  });
+
+  marker.setMap(map);
+
+  //// 선그리기
+  var pos1 = {lat: 37.463965, lng: 126.955160};
+  var pos2 = {lat: 37.463651, lng: 126.954269};
+  var pos3 = {lat: 37.463841, lng: 126.954022};
+
+  var myTrip = [pos1, pos2, pos3];
+  var flightPath = new google.maps.Polyline({
+    path:myTrip,
+    strokeColor:"#0000FF",
+    strokeOpacity:0.8,
+    strokeWeight:2
+  });
+
+  flightPath.setMap(map);
+}
+
+
+
---
+++ 2018/javascripts/main.js
@@ -0,0 +1,63 @@
+
+/////size reset ///////////////////
+
+let reset_all_size = function(){
+
+  let td_height = parseInt($( window ).width() * 0.2) // 윈도우 값의 0.2만큼을 한 셀의 크기로 설정
+  let table_width = td_height*4;  // 테이블은 4개의 셀 크기
+
+  $("table").css("width",  table_width)
+
+  $(".top_cell").css("width",  td_height)
+                .css("height",  td_height)
+
+  $("#googleMap").css("width",  td_height*2)
+                .css("height",  td_height*2)
+
+  $("td").css("width",  td_height)
+        .css("height",  td_height)
+}
+
+
+/////제이쿼리 실행 ///////////////////
+
+  $(document).ready(function(){
+
+    reset_all_size();
+
+    $( window ).resize(function() {
+      reset_all_size();
+    }) 
+
+  });
+
+
+/////menu button ///////////////////
+
+  var menu1 = function (){
+    window.scrollTo(0, $("#happy").position().top); // x, y
+  }
+
+  var menu5 = function (){
+    window.scrollTo(0, $("#SAD").position().top); // x, y
+  }
+
+
+
+/////scroll top button ///////////////////
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
